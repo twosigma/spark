452c6f95254c695932e9f31435a27fe55f71e3ba
Hamel Ajay Kothari <hkothari@twosigma.com>
2016-10-06 20:52:13 -0400

    Initial TS commit
    (cherry picked from commit efc59a11c2dbfc038174ed13da937b30820693ec)
    (cherry picked from commit 719a295fc04f6a770f866d9f499ee20563a0feef)
    (cherry picked from commit 1fad387d797456e83958aea872b5c94d3c6661f6)
    (cherry picked from commit a4071ba63a6105ac871b38fe3c77b5722d4bd229)
    (cherry picked from commit d1321c01b998d9c5fa40ff19073683a54a0db2dd)

---
+++ make-default-mca-distributions.sh
@@ -0,0 +1,2 @@
+./make-distribution.sh --tgz --skip-java-test --scala-version 2.11 --name hadoop-provided-scala2.11  -Phadoop-2.6,hadoop-provided,hive
+./make-distribution.sh --tgz --skip-java-test --scala-version 2.10 --name hadoop-provided-scala2.10  -Phadoop-2.6,hadoop-provided,hive
