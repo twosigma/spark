e9d911ed255c0cd285b73a4ba7ea2b0d80f666d7
Matthew Forsyth <andalucien@Convergence.local>
2017-04-18 09:36:15 -0400

    Change Cook patch to work with Spark 2.1.0.
    "cook" is now an optional profile when building spark, much like "mesos".

--- cook/src/main/scala/org/apache/spark/scheduler/cluster/cook/RsyncServer.scala
+++ cook/src/main/scala/org/apache/spark/scheduler/cluster/cook/RsyncServer.scala
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-package org.apache.spark.scheduler
+package org.apache.spark.scheduler.cluster.cook
 
 import java.io.{File, PrintWriter}
 import java.nio.file.Files
