2f29d5b4edca8d6e6ac24761fb29621dc38fcb5a
Wenbo Zhao <wenbo.fisher.zhao@gmail.com>
2017-06-04 18:06:55 -0400

    Fix application id to use correct cook application id for enabling ssl
    (cherry picked from commit 0063b83ffae6be26f227ea47211c396d693b965e)
    (cherry picked from commit 1e2a60e50f35f626478446dae9b2f124b01b2859)
    (cherry picked from commit f5f842c5a697b93bca43b7fbf87733d8f705d4c9)
    (cherry picked from commit 3c56e87a53bf8da7a79e41cf9f40bfe2af140285)
    (cherry picked from commit eb5765b062febba0e884c2b50fd92ad33b69f1e3)

--- resource-managers/mesos/src/main/scala/org/apache/spark/scheduler/cluster/mesos/MesosCoarseGrainedSchedulerBackend.scala
+++ resource-managers/mesos/src/main/scala/org/apache/spark/scheduler/cluster/mesos/MesosCoarseGrainedSchedulerBackend.scala
@@ -264,6 +264,8 @@ private[spark] class MesosCoarseGrainedSchedulerBackend(
     val uri = conf.getOption("spark.executor.uri")
       .orElse(Option(System.getenv("SPARK_EXECUTOR_URI")))
 
+    val cookAppId = conf.get("spark.cook.applicationId", applicationId())
+
     if (uri.isEmpty) {
       val executorSparkHome = conf.getOption("spark.mesos.executor.home")
         .orElse(sc.getSparkHome())
@@ -278,7 +280,7 @@ private[spark] class MesosCoarseGrainedSchedulerBackend(
         s" --executor-id $taskId" +
         s" --hostname ${executorHostname(offer)}" +
         s" --cores $numCores" +
-        s" --app-id $appId")
+        s" --app-id $cookAppId")
     } else {
       // Grab everything to the first '.'. We'll use that and '*' to
       // glob the directory "correctly".
@@ -291,7 +293,8 @@ private[spark] class MesosCoarseGrainedSchedulerBackend(
         s" --hostname ${executorHostname(offer)}" +
         s" --cores $numCores" +
         s" --app-id $appId")
-      command.addUris(CommandInfo.URI.newBuilder().setValue(uri.get).setCache(useFetcherCache))
+        s" --app-id $cookAppId")
+      command.addUris(CommandInfo.URI.newBuilder().setValue(uri.get))
     }
 
     conf.getOption("spark.mesos.uris").foreach(setupUris(_, command, useFetcherCache))
