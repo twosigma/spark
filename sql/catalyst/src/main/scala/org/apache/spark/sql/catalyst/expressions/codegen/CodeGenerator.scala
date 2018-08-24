831ea82fede247a595e800ddae95fe5d545bd13a
Sean Owen <sean.owen@databricks.com>
2018-08-23 21:36:53 -0700

    [SPARK-25029][BUILD][CORE] Janino "Two non-abstract methods ..." errors
    Update to janino 3.0.9 to address Java 8 + Scala 2.12 incompatibility. The error manifests as test failures like this in `ExpressionEncoderSuite`:
    
    ```
    - encode/decode for seq of string: List(abc, xyz) *** FAILED ***
    java.lang.RuntimeException: Error while encoding: org.codehaus.janino.InternalCompilerException: failed to compile: org.codehaus.janino.InternalCompilerException: Compiling "GeneratedClass": Two non-abstract methods "public int scala.collection.TraversableOnce.size()" have the same parameter types, declaring type and return type
    ```
    
    It comes up pretty immediately in any generated code that references Scala collections, and virtually always concerning the `size()` method.
    
    Existing tests
    
    Closes #22203 from srowen/SPARK-25029.
    
    Authored-by: Sean Owen <sean.owen@databricks.com>
    Signed-off-by: Xiao Li <gatorsmile@gmail.com>
    (cherry picked from commit 9b6baeb7b9f73e1a38581f481ea7232db712deb8)

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/expressions/codegen/CodeGenerator.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/expressions/codegen/CodeGenerator.scala
@@ -1389,7 +1389,7 @@ object CodeGenerator extends Logging {
     evaluator.setParentClassLoader(parentClassLoader)
     // Cannot be under package codegen, or fail with java.lang.InstantiationException
     evaluator.setClassName("org.apache.spark.sql.catalyst.expressions.GeneratedClass")
-    evaluator.setDefaultImports(Array(
+    evaluator.setDefaultImports(
       classOf[Platform].getName,
       classOf[InternalRow].getName,
       classOf[UnsafeRow].getName,
@@ -1404,7 +1404,7 @@ object CodeGenerator extends Logging {
       classOf[TaskContext].getName,
       classOf[TaskKilledException].getName,
       classOf[InputMetrics].getName
-    ))
+    )
     evaluator.setExtendedClass(classOf[GeneratedClass])
 
     logDebug({
